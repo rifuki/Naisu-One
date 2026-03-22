eb213d3f86c7a3a01d8821eaed6ea4b8b24001da
rifuki <mail@rifuki.dev>
2026-03-22 22:15:02 +0700

    chore(frontend): delete dead src/config/ (inlined into providers)

--- naisu-frontend/src/config/solana.ts
+++
@@ -1,5 +0,0 @@
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-
-export const SOLANA_ENDPOINT = "https://api.devnet.solana.com";
-
-export const wallets = [new PhantomWalletAdapter()];
